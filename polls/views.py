55d7c21482bf427ee2d29fe32ab838604513b344
QQ565253906 <565253906@qq.com>
2018-04-18 23:38:29 +0800

    2.5.3--(1)  part3
    Write views that actually do something : access database

--- polls/views.py
+++ polls/views.py
@@ -4,8 +4,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+# def index(request):
+    # return HttpResponse("Hello, world. You' re at the polls index. ")
+from django. http import HttpResponse
+from . models import Question
 def index(request):
-    return HttpResponse("Hello, world. You' re at the polls index. ")
+    latest_question_list = Question. objects. order_by('-pub_date' )[: 5]
+    output = ' , ' . join([q. question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+# Leave the rest of the views (detail, results, vote) unchanged    
 def detail(request, question_id):
     return HttpResponse("You' re looking at question %s. " % question_id)
 def results(request, question_id):
